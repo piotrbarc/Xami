66ffefe0594c91dd36a4adbcd39605d91a66767e
piotrbarc <43997971+piotrbarc@users.noreply.github.com>
2019-06-29 14:07:42 +0200

    Add project files.

---
+++ Xami/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xami
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		int count = 0;
+		void DodajOdstrzal(object sender, System.EventArgs e)
+		{
+			count++;
+			App.Current.MainPage = new DodajOdstrzal();
+		}
+
+
+	
+	}
+
+}
